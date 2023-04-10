edd143f20577f93c6c41f437ea8c1e0bc09961fd
MrTowers <dawmc52@gmail.com>
2023-04-10 22:32:28 +0200

    aktualizacja

--- js/main.js
+++ js/main.js
@@ -1,6 +1,33 @@
 "use strict";
 const input = document.getElementById("inp");
 const output = document.getElementById("out");
+class Result {
+    constructor(value) {
+        this.value = value;
+    }
+    unwrap() {
+        return this.value;
+    }
+    or(val) {
+        if (this.value == null || undefined) {
+            return val;
+        }
+        else {
+            return this.value;
+        }
+    }
+}
+function clean() {
+    input.value = "";
+}
+function getZapis() {
+    return new Result(localStorage.getItem("zapis"));
+}
+input.value = getZapis().or("");
+input.addEventListener("input", () => {
+    localStorage.setItem("zapis", input.value);
+    console.log("hi");
+});
 function exec() {
     const names = input.value.split("\n");
     //triming
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,42 @@
 const input = document.getElementById("inp") as HTMLTextAreaElement;
 const output = document.getElementById("out") as HTMLTextAreaElement;
 
+class Result <T> {
+	value: T | null;
+
+	constructor(value: T | null) {
+		this.value = value;
+	}
+
+	unwrap(): T {
+		return this.value!;
+	}
+
+	or(val: T): T {
+		if (this.value == null || undefined) {
+			return val;
+		}
+		else {
+			return this.value;
+		}
+	}
+}
+
+function clean() {
+	input.value = "";
+}
+
+function getZapis(): Result<string> {
+	return new Result<string>(localStorage.getItem("zapis"));
+}
+
+input.value = getZapis().or("");
+
+input.addEventListener("input", () => {
+	localStorage.setItem("zapis", input.value);
+	console.log("hi");
+});
+
 function exec() {
 	const names = input.value.split("\n");
 
